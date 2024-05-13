1d14b1b8c2ab92a649a6f0eb48a2d90d3debf084
cimosky <145985822+cimosky@users.noreply.github.com>
2024-05-13 16:21:20 +0200

    css und zeitanpassungen

--- js/script.js
+++ js/script.js
@@ -17,14 +17,72 @@ async function fetchData(url) {
 
 fetchData(url);
 
-//gespeicherte Daten sind in der Variable data gespeichert
-async function init() {
-    let response = await fetch(url);
-    data = await response.json();
-    console.log(data); 
+
+
+function convertSunsetTo24Hour(timeString) {
+  let [hours, minutes] = timeString.split(':');
+  minutes = minutes.replace(' PM', '');
+  if (hours !== '12') {
+      hours = parseInt(hours, 10) + 12;
+  }
+  return `${hours}:${minutes}`;
 }
 
-init();
+function subtractMinutesFromTime(timeString, minutesToSubtract) {
+  const [hours, minutes] = timeString.split(':');
+  const date = new Date();
+  date.setHours(hours);
+  date.setMinutes(minutes - minutesToSubtract);
+
+  return `${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
+}
+
+// Funktion zum Entfernen von Sekunden aus der Zeitangabe
+function removeSeconds(timeString) {
+  const [hours, minutes] = timeString.split(':');
+  return `${hours}:${minutes}`;
+}
+
+async function init() {
+  let response = await fetch(url);
+  let data = await response.json();
+
+  // Konvertieren der Sunset-Zeit und Speichern als Ursprungswert
+  const originalSunsetTime = convertSunsetTo24Hour(data[0]['sunset']);
+  document.getElementById('sunset').innerHTML = originalSunsetTime;
+
+  // Sonnenaufgangszeit ohne Sekunden formatieren und anzeigen
+  const formattedSunriseTime = removeSeconds(data[0]['sunrise']);
+  document.getElementById('sunrise').innerHTML = formattedSunriseTime;
+
+  // Event Listener für die Buttons
+  document.querySelectorAll('.button-section').forEach(button => {
+      button.addEventListener('click', function() {
+          document.querySelectorAll('.button-section').forEach(btn => btn.classList.remove('active'));
+          this.classList.add('active');
+
+          const minutes = parseInt(this.querySelector('.time').textContent, 10);
+          const newTime = subtractMinutesFromTime(originalSunsetTime, minutes);
+          document.getElementById('sunset').innerHTML = newTime;
+      });
+  });
+
+  // Standardmäßig 30 Minuten subtrahieren und Button markieren
+  const defaultButton = document.querySelector('.button-section'); // Stellen Sie sicher, dass dies der 30-Minuten-Button ist.
+  defaultButton.classList.add('active');
+  const defaultMinutes = parseInt(defaultButton.querySelector('.time').textContent, 10);
+  const newTime = subtractMinutesFromTime(originalSunsetTime, defaultMinutes);
+  document.getElementById('sunset').innerHTML = newTime;
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+
+
 
 const linienDiagramm = document.querySelector('#linienDiagramm');
 
